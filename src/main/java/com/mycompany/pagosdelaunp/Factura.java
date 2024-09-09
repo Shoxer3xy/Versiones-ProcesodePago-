7328033c5c82ea09f31e46d598911847b64b1750
Johnfer <narutoonline911@gmail.com>
2024-09-09 13:03:33 -0500

    Version Final de la Clase Factura

--- src/main/java/com/mycompany/pagosdelaunp/Factura.java
+++ src/main/java/com/mycompany/pagosdelaunp/Factura.java
@@ -10,16 +10,19 @@ package com.mycompany.pagosdelaunp;
  */
 public class Factura {
       private final Pago pago;
-   
+    private final String fecha;
 
     public Factura(Pago pago) {
         this.pago = pago;
-       
+        this.fecha = java.time.LocalDate.now().toString();
     }
 
     public void imprimirFactura() {
+        System.out.println("Factura:");
+        System.out.println("Fecha: " + fecha);
         System.out.println("Código de estudiante: " + pago.getCodigoEstudiante());
         System.out.println("Monto pagado: S/" + pago.getMonto());
+        System.out.println("Tipo de tarjeta: " + pago.getTipoTarjeta());
         System.out.println("Código de transacción: " + pago.getCodigoTransaccion());
     }
 }
